d5be1ee2cb5deb6eb65dd496a774d496b2c8a341
Jack Stewart <54085867+Stewie4848@users.noreply.github.com>
2021-05-11 22:36:55 +1000

    Add timer to game

--- app/src/main/java/com/example/educationgame/Game.java
+++ app/src/main/java/com/example/educationgame/Game.java
@@ -1,6 +1,26 @@
 package com.example.educationgame;
 
 public class Game {
+    public String difficulty;
+    private int seconds;
+    public int time_setting;
 
+    Game() {seconds = time_setting; }
+
+    public void setDifficulty(String d) {
+        difficulty = d;
+    }
+
+    public void setTime_setting(int time) {
+        seconds = time;
+    }
+
+    void tick() {
+        --seconds;
+    }
+
+    public String getSeconds() {
+        return Integer.toString(seconds);
+    }
     
 }
--- app/src/main/java/com/example/educationgame/GameActivity.java
+++ app/src/main/java/com/example/educationgame/GameActivity.java
@@ -3,14 +3,45 @@ package com.example.educationgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+    private Game game = new Game();
+    private boolean isRunning;
+    private TextView timer;
+    private TextView question;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+//        Game game = new Game();
+
+        timer = findViewById(R.id.timer);
+        question = findViewById(R.id.question);
+        game.setTime_setting(60);
+
+        enableTimer();
+
+
+
     }
 
-
+    private void enableTimer() {
+        isRunning = true;
+        handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (isRunning) {
+                    game.tick();
+                    timer.setText(game.getSeconds());
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        });
+    }
 }
